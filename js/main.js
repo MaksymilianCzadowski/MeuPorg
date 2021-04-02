c2674893c51a7831d4dd8672c0219b56db988a1e
Corsicaa <59146570+Corsicaa@users.noreply.github.com>
2021-04-02 17:43:22 +0200

    zone + mouvement

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var scene, camera, renderer, mesh;
+var meshFloor;
+var keyboard = {};
+var player = { heigth:1.8, speed:0.2 };
 
 function init() {
     scene = new THREE.Scene();
@@ -11,8 +14,15 @@ function init() {
 
     scene.add(mesh);
 
-    camera.position.set(0,0,-2)
-    camera.lookAt(new THREE.Vector3(0,0,0));
+    meshFloor = new THREE.Mesh(
+        new THREE.PlaneGeometry(25,50,25,50),
+        new THREE.MeshBasicMaterial({wireframe: true})
+    );
+    meshFloor.rotation.x -= Math.PI / 2; 
+    scene.add(meshFloor);
+
+    camera.position.set(0, player.heigth, -5)
+    camera.lookAt(new THREE.Vector3(0, player.heigth, 0));
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(1280,720);
@@ -25,10 +35,45 @@ function init() {
 function animate() {
     requestAnimationFrame(animate)
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02; 
+    if(keyboard[90]){ // W key
+		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+	}
+	if(keyboard[83]){ // S key
+		camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+	}
+	if(keyboard[81]){ // A key
+		// Redirect motion by 90 degrees
+		camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
+	}
+	if(keyboard[68]){ // D key
+		camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
+	}
+
+    
+
     renderer.render(scene, camera)
+    
+}
+
+function KeyDown(event) {
+    
+    keyboard[event.keyCode] = true;
+
 
 }
+
+function KeyUp(event) {
+    
+    keyboard[event.keyCode] = false ;
+
+}
+
+
+window.addEventListener('keydown', KeyDown);
+window.addEventListener('keyup', KeyUp);
 
 window.onload = init;
