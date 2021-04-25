ea0475c337e701f1ddc6d7df3d445ae51af9b969
Corsicaa <59146570+Corsicaa@users.noreply.github.com>
2021-04-25 16:03:44 +0200

    LAST VERSION 2.5

--- propre/js/mob.js
+++ propre/js/mob.js
@@ -1,4 +1,4 @@
-import {sphereBody,scene, monkeyBox, monkeyBoxMesh} from './main.js'
+import {sphereBody,scene, monkeyBox} from './main.js'
 import {lostLife} from './life.js'
 import { playSound } from './playsound.js';
 
@@ -7,7 +7,7 @@ export let mob;
 export function spawn() {
     new THREE.GLTFLoader().load('./model/singe.glb', function (result) {    
         mob = result.scene;
-        mob.position.set(-5, 0, -5);
+        mob.position.set(-15, 0, -5);
         mob.rotation.y += 3.2
         mob.scale.set(5, 5, 5);
         scene.add(mob);
@@ -50,7 +50,6 @@ export function deplacementMob() {
             mob.position.z -= speedMob
         }
         mob.lookAt(sphereBody.position.x, 0, sphereBody.position.z)
-        monkeyBox.position.set(mob.position.x, mob.position.y, mob.position.z);
-        monkeyBoxMesh.position.set(mob.position.x, mob.position.y, mob.position.z)
+        monkeyBox.position.set(mob.position.x, mob.position.y +2.2, mob.position.z);
     }
 }
